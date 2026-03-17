cb5abeac0dd126d234141f9dfec72b8f500b89bb
larakielcode <4220069+larakielcode@users.noreply.github.com>
2026-03-18 01:29:36 +0800

    Added the necessary db and site settings to the config file

--- includes/config.php
+++ includes/config.php
@@ -1,1 +1,20 @@
 <?php
+
+return [
+    'database' => [
+        'db_host' => 'db',
+        'db_name' => 'timetrackerdbv2',
+        'db_user' => 'root',
+        'db_pass' => 'test1234',
+        'charset' => 'utf8mb4',
+    ],
+    'pdo_options' => [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ],
+    'app_details' => [
+        'app_name' => 'OJT Timetracker',
+        'app_tzone' => 'Asia/Manila',
+    ],
+];
