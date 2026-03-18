1e14d999ff1d30dff9e4daf58f2e213ba33378b2
larakielcode <4220069+larakielcode@users.noreply.github.com>
2026-03-19 00:42:31 +0800

    updated the index.php

--- dist/index.php
+++ dist/index.php
@@ -1,9 +1,14 @@
 <?php
 
-$tes = require __DIR__ . '/../includes/login.inc.php';
+include __DIR__ . '/../includes/general.inc.php';
+
+$test = loginErrors();
+
+
+if (!isset($_SESSION['logged_user'])) {
+    include __DIR__ . '/views/login.view.php';
+}
 
 //use this template to instantiate the user
 //$test = new User('aldin', 's', 'moreno', 'usjr', 100, 2, '', '', '', '', 'test@omegahms.com', 'hahapassword', 'admin');
 //$test->getUser();
-
-var_dump($tes);
