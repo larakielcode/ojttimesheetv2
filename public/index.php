e7249fe37a7da0d566c98f38a0d7a7b1b9ecbcc7
larakielcode <4220069+larakielcode@users.noreply.github.com>
2026-03-21 00:05:00 +0800

    added an autoloader function

--- public/index.php
+++ public/index.php
@@ -1,11 +1,8 @@
 <?php
 const BASE_PATH = __DIR__ . '/../';
 require BASE_PATH . 'includes/functions.php';
-
-spl_autoload_register(function ($class) {
-    dd($class);
-});
-
+require base_path('includes/autoloader.php');
 require base_path('includes/router.php');
+
 
 routeToController($uri, $routes);
