54aed9628daf3712824a0127919c6dd1a8a9b698
larakielcode <4220069+larakielcode@users.noreply.github.com>
2026-03-22 19:32:22 +0800

    added updates to db and login class

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Core;
 
+use http\Exception\RuntimeException;
 use PDO;
 
 // I am marking this class as final so you know you dont want to extend here :P
@@ -31,7 +32,6 @@ final class Database
             $dsn = "mysql:host={$db['db_host']};dbname={$db['db_name']};charset={$db['charset']}";
 
             self::$instance = new PDO($dsn, $db['db_user'], $db['db_pass'], $config['pdo_options'] ?? []);
-            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $e) {
             die("Connection to db failed: " . $e->getMessage());
         }
@@ -41,6 +41,10 @@ final class Database
 
     public static function query(string $query, $params = [])
     {
+        if (self::$instance === null) {
+            throw new RuntimeException('Database must be initialized.');
+        }
+
         $statement = self::$instance->prepare($query);
         $statement->execute($params);
         return $statement;
