6a86678e6fb733c18099e77d68ba6ef9f2755482
larakielcode <4220069+larakielcode@users.noreply.github.com>
2026-03-22 13:15:57 +0800

    added a validation class

---
+++ app/Core/Validation.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+class Validation
+{
+    public static function checkEmpty(array $items): string
+    {
+        foreach ($items as $item) {
+
+        }
+    }
+}
