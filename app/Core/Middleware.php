18f176d67a3ffdb80626d808e08602cf8e012d34
larakielcode <4220069+larakielcode@users.noreply.github.com>
2026-03-22 11:39:52 +0800

    update middleware

--- app/Core/Middleware.php
+++ app/Core/Middleware.php
@@ -1,1 +1,14 @@
 <?php
+declare(strict_types=1);
+
+namespace App\Core;
+
+class Middleware
+{
+
+    public function only(string $value): void
+    {
+        $value === 'guest' ? $this->guest() : $this->auth();
+    }
+
+}
