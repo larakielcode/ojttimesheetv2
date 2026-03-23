0734d737182f27530fc921e1b4b3da28f5b2fec2
larakielcode <4220069+larakielcode@users.noreply.github.com>
2026-03-23 09:53:26 +0800

    added some comments on the routes

--- routes/routes.php
+++ routes/routes.php
@@ -1,11 +1,11 @@
 <?php
 
 // Routes for Login
-$router->get('/', 'app/Controllers/login/index.php');
-$router->post('/', 'app/Controllers/login/login.php');
+$router->get('/', 'app/Controllers/login/index.php'); # view the login page
+$router->post('/', 'app/Controllers/login/login.php'); # logged the user in
 
 // Routes for Dashboard
-$router->get('/dashboard', 'app/Controllers/dashboard/index.php');
+$router->get('/dashboard', 'app/Controllers/dashboard/index.php'); # view the dashboard
 
 
 // this is for migrations
