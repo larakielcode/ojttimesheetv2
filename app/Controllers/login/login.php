b4c0cd1db3cf56933f5f9259f61c958ed054c69d
larakielcode <4220069+larakielcode@users.noreply.github.com>
2026-03-23 09:27:58 +0800

    updated the login on login and login class

--- app/Controllers/login/login.php
+++ app/Controllers/login/login.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Core\Database;
+use App\Core\Login;
 use App\Core\Validation;
 
 $config = require basePath('config/config.php');
@@ -9,21 +10,28 @@ $connection = Database::getConnection($config);
 $errors = [];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+    $email = trim($_POST['email']) ?? '';
+    $password = $_POST['password'] ?? '';
 
     if (Validation::isEmpty($email) || Validation::isEmpty($password)) {
         $errors['login'] = "Email and password are required.";
     }
 
-    // if all validation pass then call the login class - to be refactor
+    if (empty($errors)) {
 
-    $user = Database::query("SELECT password FROM users_login WHERE email = :email", [$email])->fetch();
+        try {
+            
+            $user = Database::query("SELECT users_id, password FROM users_login WHERE email = :email", ['email' => $email])->fetch();
 
-    if ($user && password_verify($password, $user['password'])) {
-        dd("User found log the user in");
-    } else {
-        $errors['login'] = "Enter a valid username or password.";
+            if ($user && password_verify($password, $user['password'])) {
+                $loggedUser = new Login($user['users_id'], $email, $connection);
+            } else {
+                $errors['login'] = "Enter a valid username or password.";
+            }
+        } catch (\PDOException $e) {
+
+            $errors['login'] = "A database error occurred.";
+        }
     }
 }
 
