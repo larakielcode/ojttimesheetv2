318ae9b8b0fad5506f6e25c66602e988cadd5568
larakielcode <4220069+larakielcode@users.noreply.github.com>
2026-04-08 06:42:50 +0800

    updated the dashboard

--- app/Controllers/dashboard/index.php
+++ app/Controllers/dashboard/index.php
@@ -5,6 +5,6 @@ use App\Core\Session;
 Session::checkIdle(); # this will check if the user is inactive for 30mins
 
 
-echo "Hello! Welcome to the dashboard <br><br>{$_SESSION['logged_on_user']['email']}";
+//echo "Hello! Welcome to the dashboard <br><br>{$_SESSION['logged_on_user']['email']}";
 
 require views('portal.view.php');
