0b150719de336192b81c9d700e2fdf0e2de97027
larakielcode <4220069+larakielcode@users.noreply.github.com>
2026-05-02 21:43:53 +0800

    put comments on routes for readability

--- routes/routes.php
+++ routes/routes.php
@@ -1,21 +1,23 @@
 <?php
 
-// Routes for Login
-$router->get('/', 'app/Controllers/login/index.php')->only('guest'); # view the login page
+# Routes for Login
+$router->get('/', 'app/Controllers/login/index.php')->only('guest'); # view the login page | login.view.php
 $router->post('/login', 'app/Controllers/login/login.php'); # logged the user in
 
-$router->get('/login', 'app/Controllers/login/index.php')->only('guest');
+$router->get('/login', 'app/Controllers/login/index.php')->only('guest'); # login link login.view.php
+
+# Routes for Logout
 $router->get('/logout', 'app/Controllers/logout.php')->only('auth');
 $router->post('/logout', 'app/Controllers/logout.php');
 
 
-// Routes for Dashboard
+# Routes for Dashboard
 $router->get('/dashboard', 'app/Controllers/dashboard/index.php')->only('auth'); # view the dashboard
 
 
-// Routes for sites
+# Routes for sites
 $router->get('/sites/create', 'app/Controllers/sites/index.php')->only('auth');
 $router->post('/sites/create', 'app/Controllers/sites/create.php');
 
-// this is for migrations
+# this is for migrations
 $router->get('/migrations', 'migrations/migrations.php');
